b40d6ce2251584728a09627bf4a65004b669cebd
safishub <safi.sadykhova@student.htw-berlin.de>
2025-03-20 14:53:53 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ namespace NotesAPP
 {
     internal class Program
     {
-        private static List<Note> notes = []; // List to store all notes
-        private static JsonBase64FileData fileHandler = new(); // Handler for file operations
-        private static CancellationTokenSource cancellationTokenSource = new(); // Managing background tasks
+        private static List<Note> notes = []; 
+        private static JsonBase64FileData fileHandler = new(); 
+        private static CancellationTokenSource cancellationTokenSource = new(); background tasks
 
-        private static bool notificationIsEnabled = false; // Toggle for reminders
+        private static bool notificationIsEnabled = false; // reminders
         private static string fileName = "notes.json";
 
         private static void Main(string[] args)
@@ -19,22 +19,21 @@ namespace NotesAPP
                 notes = fileHandler.LoadFromFile<List<Note>>(fileName);
             }
 
-            // Start the background task for checking notifications
+        
             var notificationTask = CheckNotificationsAsync(cancellationTokenSource.Token);
 
-            // Main menu loop
+            
             while (true)
             {
                 Console.Clear();
 
                 DisplayMenu();
 
-                // Get user's choice
+                
                 string choice = Console.ReadLine();
 
                 Console.Clear();
 
-                // Handle user's choice
                 switch (choice)
                 {
                     case "1":
@@ -68,11 +67,10 @@ namespace NotesAPP
                         break;
                 }
 
-                // Sort notes and save
+                
                 notes.Sort();
                 fileHandler.SaveToFile(notes, fileName);
 
-                // Wait for user input before continuing
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
             }
@@ -92,14 +90,12 @@ namespace NotesAPP
             Console.Write("Choose an option: ");
         }
 
-        // Toggles the reminders on or off.
         private static void ToggleReminders()
         {
             notificationIsEnabled = !notificationIsEnabled;
             Console.WriteLine($"Reminders {(notificationIsEnabled ? "enabled" : "disabled")}.");
         }
 
-        // Exits the program gracefully.
         private static void ExitProgram()
         {
             cancellationTokenSource.Cancel(); // Stop the background task
@@ -111,47 +107,47 @@ namespace NotesAPP
         {
             Note note = new(); // Create a new note object
 
-            // Prompt the user to enter the note's title
+            
             Console.Write("Enter title: ");
             note.title = Console.ReadLine();
 
-            // Prompt the user to enter the note's category
+            
             Console.Write("Enter category: ");
             note.category = Console.ReadLine();
 
-            // Prompt the user to enter the reminder time in a specific format
+            
             Console.Write("Enter reminder time (HH:mm dd-MM-yyyy): ");
             DateTime date = DateTime.MinValue;
 
-            // Try to parse the input into a DateTime object
+            
             bool success = DateTime.TryParseExact(Console.ReadLine(), "HH:mm dd-MM-yyyy", CultureInfo.CurrentCulture,
                 DateTimeStyles.AllowLeadingWhite, out date);
 
-            // Set the reminder time and display success/failure message
+            
             note.reminderTime = date;
             Console.WriteLine($"{(success ? "Success" : "Failure")}: date set to {date}");
 
-            // Prompt the user to enter the note's content
+            
             Console.Write("Enter content: ");
             note.content = Console.ReadLine();
 
-            // Add the note to the list
+            
             notes.Add(note);
 
-            // Confirm that the note has been added
+            
             Console.WriteLine("Note added.");
         }
 
         static void ShowAllNotes()
         {
-            // Check if there are any notes
+            
             if (notes.Count == 0)
             {
                 Console.WriteLine("No notes found.");
                 return;
             }
 
-            // Display all notes
+            
             Console.WriteLine(new string('-', 20));
             for (int i = 0; i < notes.Count; i++)
             {
@@ -161,13 +157,13 @@ namespace NotesAPP
                 Console.WriteLine($"Category: {note.category}");
                 Console.WriteLine($"Reminder time: {note.reminderTime}");
                 Console.WriteLine($"Content: {note.content}");
-                Console.WriteLine(new string('-', 20)); // Separator line
+                Console.WriteLine(new string('-', 20)); // Separation
             }
         }
 
         static void ShowCategory()
         {
-            // Get a list of unique categories
+            
             List<string> categories = GetCategories();
             int idOfCategory = 0;
 
@@ -177,26 +173,24 @@ namespace NotesAPP
                 return;
             }
 
-            // Prompt the user to select a category by number
+            
             Console.WriteLine("Enter the category number:");
 
-            // Display all categories
+            
             for (int i = 0; i < categories.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {categories[i]}");
             }
 
-            // Try to parse the user's input into an integer
             bool success = int.TryParse(Console.ReadLine(), out idOfCategory);
 
-            // Validate the input
             if (idOfCategory <= 0 || idOfCategory > categories.Count || !success)
             {
                 Console.WriteLine("Category not found.");
                 return;
             }
 
-            // Display all notes in the selected category
+            // all notes in the selected category
             foreach (Note note in notes)
             {
                 if (note.category == categories[idOfCategory - 1])
@@ -214,11 +208,10 @@ namespace NotesAPP
         {
             int id = 0;
 
-            // Prompt the user to enter the note's serial number
+            
             Console.Write("Enter the note's serial number: ");
             bool success = int.TryParse(Console.ReadLine(), out id);
 
-            // Validate the input
             if (id <= 0 || id > notes.Count || !success)
             {
                 Console.WriteLine("Note not found.");
@@ -228,28 +221,22 @@ namespace NotesAPP
             // Get the note to edit
             Note note = notes[id - 1];
 
-            // Edit the title
             Console.WriteLine($"Title: {note.title} - replace with (leave blank to keep current):");
             string title = Console.ReadLine();
             note.title = string.IsNullOrEmpty(title) ? note.title : title;
 
-            // Edit the category
             Console.WriteLine($"Category: {note.category} - replace with (leave blank to keep current):");
             string category = Console.ReadLine();
             note.category = string.IsNullOrEmpty(category) ? note.category : category;
 
-            // Edit the reminder time
             Console.WriteLine($"Reminder time (HH:mm dd-MM-yyyy) - replace with (leave blank to keep current):");
             DateTime date = DateTime.MinValue;
 
-            // Parse the input date
             DateTime.TryParseExact(Console.ReadLine(), "HH:mm dd-MM-yyyy", CultureInfo.CurrentCulture,
                 DateTimeStyles.AllowLeadingWhite, out date);
 
-            // Update the reminder time if a valid date was entered
             note.reminderTime = (date == DateTime.MinValue) ? note.reminderTime : date;
-
-            // Edit the content 
+ 
             Console.WriteLine($"Content: {note.content} - replace with (leave blank to keep current):");
             string content = Console.ReadLine();
             note.content = string.IsNullOrEmpty(content) ? note.content : content;
@@ -258,35 +245,31 @@ namespace NotesAPP
         }
         static void DeleteNote()
         {
-            // Prompt the user to enter the note's serial number
+
             Console.Write("Enter the note's serial number: ");
 
             int id = 0;
 
-            // Try to parse the user's input into an integer
             bool success = int.TryParse(Console.ReadLine(), out id);
 
-            // Validate the input
             if (id <= 0 || id > notes.Count || !success)
             {
                 Console.WriteLine("Note not found.");
                 return;
             }
 
-            // Remove the note at the specified index
             notes.RemoveAt(id - 1);
 
-            // Notify the user that the note has been deleted
             Console.WriteLine("Note deleted successfully.");
         }
 
         static async Task CheckNotificationsAsync(CancellationToken cancellationToken)
         {
-            while (!cancellationToken.IsCancellationRequested) // Run until cancellation is requested
+            while (!cancellationToken.IsCancellationRequested) 
             {
                 foreach (var note in notes)
                 {
-                    // If reminder time is reached, notification is enabled, and not yet notified
+                    
                     if (DateTime.Now >= note.reminderTime && !note.isNotificated && notificationIsEnabled)
                     {
                         Console.WriteLine($"\n\nNotification: {note.title} - {note.content}");
@@ -301,7 +284,7 @@ namespace NotesAPP
         static void FindNote()
         {
             Console.Write("Search by title: ");
-            string title = Console.ReadLine().ToLower(); // Convert input to lowercase for case-insensitive search
+            string title = Console.ReadLine().ToLower(); // Convert input to lowercase
 
             Console.Clear();
 
@@ -310,7 +293,6 @@ namespace NotesAPP
             {
                 Note note = notes[i];
 
-                // Check if the note's title contains the search term
                 if (note.title.ToLower().Contains(title))
                 {
                     Console.WriteLine($"Title: {note.title}");
@@ -324,14 +306,14 @@ namespace NotesAPP
 
         static List<string> GetCategories()
         {
-            List<string> listOfCategories = new(); // Create a list to store unique categories
+            List<string> listOfCategories = new(); 
 
             // Iterate through all notes
             for (int i = 0; i < notes.Count; i++)
             {
                 Note note = notes[i];
 
-                // Add the category to the list if it's not already present
+                
                 if (!listOfCategories.Contains(note.category))
                 {
                     listOfCategories.Add(note.category);
